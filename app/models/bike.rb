245dc745d62f30910b6c0a6b08999c769ff99d1c
ana frescki <karolinefrescki@gmail.com>
2025-07-11 20:25:02 +0200

    validations updated

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,6 +3,5 @@ class Bike < ApplicationRecord
   has_many :matches
   has_many :reports
 
-  validates :brand, :model, :stolen_date, :location_lost, :status, :user_id, :first_search_date, presence: true
-
+  validates :brand, :model, :stolen_date, :location_lost, :status, :first_search_date, presence: true
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,4 @@
 class Report < ApplicationRecord
   belongs_to :bike
   belongs_to :match
-
-
-  validates :match, presence: true
-  validates :bike_id, :content, presence: true
 end
