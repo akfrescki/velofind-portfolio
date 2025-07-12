8ae2abe9c7805e34e9e8bcf532e541e43a04feb6
Axel Houillier <axelhouillier@yahoo.fr>
2025-07-12 10:33:48 +0200

    created the seeds

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,5 +2,5 @@ class Match < ApplicationRecord
   belongs_to :bike
   has_one :report, dependent: :destroy
 
-  validates :marketplace_url, :match_score, :marketplace, :price, :seller, presence: true
+  validates :markeplace_url, :match_score, :marketplace, :price, :seller, presence: true
 end
