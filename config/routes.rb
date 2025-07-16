c0dc8677dddd0545c410648ac0de24dfa4a58863
Helene Gaspar <helene.gaspar@velopass.com>
2025-07-16 18:58:49 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'users#dashboard', as: :dashboard
 
-  resources :promos, only: [:index, :new, :create]
+  resources :promos, only: [:index]
 end
