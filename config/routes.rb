24ada9ce1e0dc4f1712ab3bd5c251c01176f1747
Stephen Etuokwu <stephennelson396@gmail.com>
2025-07-16 12:28:49 +0100

    Save local changes before merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :bikes, only: [:create]
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'users#dashboard', as: :dashboard
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :promos, only: [:index, :new, :create]
 end
